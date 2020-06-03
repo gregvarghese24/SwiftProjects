424d98457707e704be84d62685acfc539498e61d
Greg Varghese <46109868+gregvarghese24@users.noreply.github.com>
2020-06-03 00:10:15 -0400

    Add files via upload

---
+++ I Am Rich/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Greg Varghese on 5/28/20.
+//  Copyright © 2020 Greg Varghese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
